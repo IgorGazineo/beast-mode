6243196715344fd637b909d47ab0fc4cfd27f37a
Igor Gazineo <gaumgazineo@hotmail.com>
2025-01-10 07:27:58 -0300

    finalizando a prática 01 do módulo 03 do curso the-complete-nodejs-developer-course

--- the-complete-nodejs-developer-course/module-03/01/node-course/notes-app/app.js
+++ the-complete-nodejs-developer-course/module-03/01/node-course/notes-app/app.js
@@ -1,7 +1,7 @@
-const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes");
 
 const msg = getNotes();
 console.log(msg);
 
-console.log(validator.isURL("https/mead.io"));
+console.log(chalk.bold.inverse.rgb(255, 0, 0)("Error!"));
