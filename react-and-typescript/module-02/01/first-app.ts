f699de82b327bbee00a8eefd2491fdd1902631a4
Igor Gazio <gaumgazineo@hotmail.com>
2025-01-15 08:55:50 -0300

    finalizando a prática 01 do módulo 02 do curso react-and-typescript

--- react-and-typescript/module-02/01/first-app.ts
+++ react-and-typescript/module-02/01/first-app.ts
@@ -9,19 +9,23 @@ let isValid = true;
 
 // -------------
 
-let userID: string | number = "abc1";
+type StringOrNum = string | number;
+
+let userID: StringOrNum = "abc1";
 userID = 123;
 
 // userID = true;
 
 // let user: object;
 
-let user: {
+type User = {
   name: string;
   age: number;
   isAdmin: boolean;
   id: string | number;
 };
+
+let user: User;
 
 user = {
   name: "Max",
@@ -31,3 +35,107 @@ user = {
 };
 
 // user = {};
+
+// let hobbies: Array<string>;
+let hobbies: { name: string; age: number }[];
+
+hobbies = [{ name: "Igor", age: 37 }];
+// hobbies = [1, 2, 3]
+
+function add(a: number, b: number): number {
+  const result = a + b;
+  return result;
+}
+
+type AddFn = (a: number, b: number) => number;
+
+function calculate(a: number, b: number, calcFn: AddFn) {
+  calcFn(a, b);
+}
+
+calculate(2, 5, add);
+
+interface Credentials {
+  password: string;
+  email: string;
+}
+
+// interface Credentials {
+//   mode: string;
+// }
+
+let creds: Credentials;
+
+creds = {
+  password: "abc",
+  email: "test@gmail",
+};
+
+class AuthCredentials implements Credentials {
+  userName: string;
+  password: string;
+  email: string;
+}
+
+function login(credentials: Credentials) {}
+
+login(new AuthCredentials());
+
+// type Admin = {
+//   permission: string[];
+// };
+
+// type AppUser = {
+//   userName: string;
+// };
+
+// type AppAdmin = Admin & AppUser;
+
+interface Admin {
+  permission: string[];
+}
+
+interface AppUser {
+  userName: string;
+}
+
+interface AppAdmin extends Admin, AppUser {}
+
+let admin: AppAdmin = {
+  permission: ["login"],
+  userName: "Max",
+};
+
+type Role = "admin" | "user" | "editor";
+
+let role: Role;
+
+role = "admin";
+// role = 'abc'
+
+function performAction(action: string | number, role: Role) {
+  if (role === "admin" && typeof action === "string") {
+    // ...
+  }
+}
+
+let roles: Array<Role>;
+roles = ["admin", "editor"];
+
+type DataStorage<T> = {
+  storage: T[];
+  add: (data: T) => void;
+};
+
+const textStorage: DataStorage<string> = {
+  storage: [],
+  add(data) {
+    this.storage.push(data);
+  },
+};
+
+textStorage.add("Igor");
+textStorage.add("Luara");
+textStorage.add("Isabella");
+
+console.log(textStorage.storage);
